5748ceebfa486f8519ec15f39f8cd306929dae71
Samarth Sharma <dexxrolo@gmail.com>
2022-02-16 01:23:16 +0530

    Add: Db connections

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+
+const connectDB = require("./config/db");
+// connect to db
+connectDB();
+
 app.use(express.json({ extended: false }));
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is up on ${PORT}`));
