b22d25daf8ae9475a618e86a947f7db033de0c19
Samarth Sharma <dexxrolo@gmail.com>
2022-02-16 01:47:32 +0530

    Add: routes and model

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ const connectDB = require("./config/db");
 connectDB();
 
 app.use(express.json({ extended: false }));
+
+//Routes
+
+app.use("/", require("./routes/index"));
+app.use("/api/url", require("./routes/url"));
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is up on ${PORT}`));
---
+++ routes/url.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const validUrl = require("valid-url");
+const shortid = require("shortid");
+const config = require("config");
+
+const Url = require("../models/Url");
+
+// route: POST /api/url/shorten
+// desc: create short url
+router.post("/shortern", async (req, res, next) => {
+  const { longUrl } = req.body;
+  const baseUrl = config.get("baseUrl");
+
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(401).json("Invalid base url");
+  }
+  //Shortern logic
+  const urlCode = shortid.generate();
+  //check long url
+  if (validUrl.isUri(longUrl)) {
+    try {
+      let url = await Url.findOne({ longUrl });
+      if (url) res.json(url);
+      else {
+        const shortUrl = `${baseUrl}/${urlCode}`;
+        url = new Url({
+          longUrl,
+          shortUrl,
+          urlCode,
+        });
+        await url.save();
+        res.json(url);
+      }
+    } catch (error) {
+      console.log("Something unexpected happened:" + error);
+      res.status(500).json("Server error");
+    }
+  } else {
+    res.status(401).json("Invalid long url");
+  }
+});
+
+module.exports = router;
